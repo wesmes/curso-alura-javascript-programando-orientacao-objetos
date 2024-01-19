b79852d878220e204a822bc8140a1153dd8dda1e
Weslley Gomes da Cruz <w_gomes@protonmail.com>
2024-01-19 05:24:30 -0300

    Módulo 1 - Aula 3: Entendendo o problema do cliente

---
+++ index.js
@@ -0,0 +1,13 @@
+const clinte1 = "Ricardo";
+const clinte1CPF = 11122233309;
+const clinte1Agencia = 1001;
+const clinte1Saldo = 0;
+
+const clinte2 = "Alice";
+const clinte2CPF = 88822233309;
+const clinte2Agencia = 1001;
+const clinte2Saldo = 0;
+
+console.log(clinte1);
+console.log(clinte2);
+

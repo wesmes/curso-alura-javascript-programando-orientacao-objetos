45e4f8e46f0314e303afc08d177efb369a2e8bb1
Weslley Gomes da Cruz <w_gomes@protonmail.com>
2024-01-20 05:43:21 -0300

    Módulo 2 - Aula 7: Métodos com retorno

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ class Cliente {
 
 class ContaCorrente {
     agencia;
-    saldo;
+    #saldo = 0;
 
     sacar(valor){
-        if (this.saldo >= valor) {
-            this.saldo -= valor;
+        if (this.#saldo >= valor) {
+            this.#saldo -= valor;
+            return valor;
         }
+    }
+
+    depositar(valor){
+        if(valor <= 0) return;
+        this.#saldo += valor;
     }
 }
 
@@ -24,13 +30,12 @@ cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.saldo = 0;
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.saldo = 100;
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.sacar(200);
 
-console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(200);
+contaCorrenteRicardo.depositar(-1);
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
 
 console.log(cliente1);
 console.log(cliente2);

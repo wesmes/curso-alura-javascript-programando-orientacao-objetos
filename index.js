6eb52c349f54101ba2a7249460f4a626cafd0dc5
Weslley Gomes da Cruz <w_gomes@protonmail.com>
2024-01-22 05:49:19 -0300

    Módulo 4 - Aula 2: Null e undifined

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
-
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
 contaCorrenteRicardo.cliente = cliente1;
@@ -16,7 +12,9 @@ contaCorrenteRicardo.depositar(500);
 
 const conta2 = new ContaCorrente();
 conta2.agencia = 102;
-conta2.cliente = cliente2;
+conta2.cliente = new Cliente();
+conta2.cliente.nome = "Alice";
+conta2.cliente.cpf = 88822233309;
 
 contaCorrenteRicardo.transferir(200, conta2);
 
